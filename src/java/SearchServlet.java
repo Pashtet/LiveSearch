e8f5d0962aab65fe923ae3fc038d6280174e661f
Pavel Trofimov <paveltrfimv@gmail.com>
2017-01-30 15:11:37 +0300

    Первая версия LiveSearch через Jquery

---
+++ src/java/SearchServlet.java
@@ -0,0 +1,72 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author veerasundar.com/blog
+ *
+ */
+public class SearchServlet extends HttpServlet {
+
+
+    private static final long serialVersionUID = 1L;
+
+    public SearchServlet() {
+
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+       doPost(request,response);
+    }
+
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+            String searchText = request.getParameter("searchText");
+            String report = getWeather(searchText);
+            response.setContentType("text/plain;charset=utf-8");
+            PrintWriter out = response.getWriter();
+            out.println("" + report + "");
+            out.flush();
+            out.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SearchServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private String getWeather(String searchText) throws ClassNotFoundException, UnsupportedEncodingException {
+        Connection conn = null;
+        Statement st = null;
+        ResultSet res = null;
+        String finalSearch = "";
+        
+        try {
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/files", "netbeans", "netbeans");
+            st = conn.createStatement();
+            String s = "SELECT * FROM ps;";
+
+            res = st.executeQuery(s);
+            while (res.next()) {
+                String un = res.getString("ps_name");
+                finalSearch += un + "\n";
+            }
+            st.close();
+            conn.close();
+        }  catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        }
+        return finalSearch;//new String (finalSearch.getBytes(), "CP1251");
+    }
+}
